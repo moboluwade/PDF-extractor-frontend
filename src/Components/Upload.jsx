efc2f4c10673ba3b7e918c34f319df6f4912ed46
moboluwade <folarinboluwade.com>
2023-09-13 15:34:40 +0100

    add controlled input for input element

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 
-const upload = () => {
+const Upload = () => {
+    const [file, setFile] = useState("")
+    const onInputFileChange = (e) => {
+        setFile(e.target.file)
+        console.log(e.target.file)
+        console.log(e.target.value)
+    }
+    
     return (
         <div className="upload-wrapper">
             <div className="welcome-text dynamic-section display-text">
                 <h1>
                     Upload a pdf and extract your text
                 </h1>
+                <img src="/si" alt="" srcset="" />
+                {/* <img src={file} alt="" /> */}
             </div>
             <label className="upload-card" htmlFor="upload">
-                <label className="custom-label upload-icon" htmlFor="upload">
+                <span className="custom-label upload-icon">
                     <FileUploadIcon style={{ fontSize: "4rem" }} />
-                </label>
+                </span>
                 <button className="upload-icon-button">
-                    <p>Upload file</p>
-                    <input accept=".pdf, .jpg" type="file" name="pdfFile" id="upload" />
+                    <label className="button-label" htmlFor="upload">
+                        <p>Upload file</p>
+                    </label>
                 </button>
+                <input onChange={(e) => { onInputFileChange(e) }} value={file} accept=".pdf, .jpg" type="file" name="pdfFile" id="upload" />
                 <p>or drop a file</p>
             </label>
         </div>
     )
 }
 
-export default upload;
+export default Upload;
