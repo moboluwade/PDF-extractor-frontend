ac353a1b35a9d6be7fb7d2cf10af5339c95bdbd4
moboluwade <folarinboluwade.com>
2023-09-29 23:59:35 +0100

    refactor outgoing post request and useEffect callback

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,35 +1,9 @@
-import React, {useEffect, useLayoutEffect, useRef, useState} from "react";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import axios from "axios";
 import UploadCard from "./UploadCard";
 import DynamicSection from "./DynamicSection";
 
 const UPLOAD_ENDPOINT = `${process.env.UPLOAD_ENDPOINT_URL}`
-
-const fetchProcessedFile = async (file) => {
-    console.log("before try and catch")
-    try {
-        console.log("sending request now")
-        const formData = new FormData()
-        formData.set("pdf_content", file)
-        const response = await axios.post(UPLOAD_ENDPOINT, file, {
-            withCredentials: false,
-            headers: {
-                "Access-Control-Allow-Credentials": "false",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Content-type": "application/pdf",
-            }
-        })
-
-        const processedText = await response.json()
-        console.log("response should be delivered")
-        console.log(processedText)
-        return processedText
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 //fetch function has to do three things//
 //1. send the file across over the internet
@@ -44,26 +18,50 @@ const fetchProcessedFile = async (file) => {
 const Hero = () => {
     const [switchToITE, setSwitchToITE] = useState(false)
     const [result, setResult] = useState("PDF is loading...")
-    const [triggerFetch, setTriggerFetch] = useState(false)
     const [file, setFile] = useState("")
 
+    const fetchProcessedFile = async (file) => {
+        try {
+            const formData = new FormData()
+            formData.set("pdf_content", file)
+            const response = await axios.post(UPLOAD_ENDPOINT, formData, {
+                headers: {
+                    "Access-Control-Allow-Credentials": "true",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Content-type": "multipart/form-data",
+                }
+            })
+    
+            const processedText = await response.data()
+            return processedText
+        } catch (error) {
+            console.log(error)
+        }
+    }
     const isMounted = useRef(false)
     useLayoutEffect(
-        (file)=>{
-            if(isMounted.current){
-                const response = fetchProcessedFile(file)
-                setResult(response || "PDF is loading...")
-            }else{
-                isMounted.current = true
+        ()=>{
+            const triggerFetch = async() =>{
+                if(isMounted.current){
+                    const response = await fetchProcessedFile(file)
+                    console.log("response ", response)
+                    setResult(response || "PDF is loading...")
+                    // console.log("result: ", result)
+                }else{
+                    isMounted.current = true
+                }
             }
+            triggerFetch();
         },
-        [triggerFetch]
+        [file]
     )
 
     return (
         <div className="upload-wrapper">
             <DynamicSection switchToITE={switchToITE} result={result}/>
-            <UploadCard file={file} setTriggerFetch={setTriggerFetch} setFile={setFile} setSwitch={setSwitchToITE}/>
+            <UploadCard file={file} setFile={setFile} setSwitch={setSwitchToITE}/>
         </div>
     )
 }
